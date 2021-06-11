dd3d5bc769810e5c1668f610482c122620b4b5f2
MrTroble <nifroh56@gmail.com>
2021-06-11 11:51:43 +0200

    Added check tools

--- project.py
+++ project.py
@@ -2,6 +2,8 @@ import json
 import os
 import zipfile
 import urllib.request as url
+import re
+import traceback
 try:
     import zlib
     compression = zipfile.ZIP_DEFLATED
@@ -36,6 +38,46 @@ def packAbleFiles(file):
     return file.endswith(".json") or file.endswith(".obj") or file.endswith(".png") or file.endswith(".mcmeta") or file.endswith(".mtl")
 
 
+depCache = None
+ptrn = r"\s*cm\.register\s*\(\s*\"([^\"]+)\",((.*\),)|([^\,]+,))\s*[0-9\.f]*,([^\)]+)\)\;"
+ptrn2 = r"\s*\"([^\"]+)\""
+
+
+def check(name, pth):
+    global depCache
+    try:
+        if depCache == None:
+            depCache = []
+            with open("GIRCustomModelLoader.java") as fp:
+                groups1 = re.findall(ptrn, fp.read())
+                for cont in groups1:
+                    name = cont[0]
+                    name = os.path.basename(name) + ".json"
+                    if len(cont) == 5:
+                        scre = cont[4]
+                        rslt = re.findall(ptrn2, scre)
+                        depCache.append((name, rslt))
+        cacheLists = list(filter(lambda x: x[0] == name, depCache))
+        if len(cacheLists) > 0:
+            with open(pth) as fp:
+                jobj = json.load(fp)
+                if "textures" not in jobj:
+                    return
+                texlist = jobj["textures"]
+                dupl = []
+                for tpl in cacheLists:
+                    lst = tpl[1]
+                    for x in range(0, len(lst), 2):
+                        if lst[x] not in texlist:
+                            if lst[x] in dupl: continue
+                            dupl.append(lst[x])
+                            print("Warning {} not found in texture list of {}, please make sure it uses this texture as it is needed! [Example: {}]".format(
+                                lst[x], name, lst[x+1]))
+    except:
+        traceback.print_exc()
+        print("Error in cmd!")
+
+
 def pack():
     vers = "none"
     if "version" not in projectJson:
@@ -47,6 +89,7 @@ def pack():
             for file in files:
                 pth = os.path.join(root, file)
                 if packAbleFiles(file):
+                    check(file, pth)
                     if (os.path.sep + "models" + os.path.sep) in root and file.endswith(".png"):
                         zf.write(pth, compress_type=compression,
                                  arcname="assets/minecraft/textures/" + file)
@@ -79,6 +122,10 @@ def update():
 
     with zipfile.ZipFile("dl.zip") as zf:
         for zi in zf.filelist:
+            if zi.filename.endswith("GIRCustomModelLoader.java"):
+                with zf.open(zi, "r") as zfp:
+                    with open("GIRCustomModelLoader.java", "wb") as nfp:
+                        nfp.write(zfp.read())
             if "/src/main/resources/assets/" in zi.filename:
                 fname = zi.filename.split("/src/main/resources/")[1]
                 if zi.is_dir():
@@ -96,7 +143,22 @@ def update():
 if first:
     update()
 
-commands = {"update": update, "pack": pack}
+def checkAll():
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            if packAbleFiles(file):
+                pth = os.path.join(root, file)
+                check(file, pth)
+
+def checkcmd():
+    name = input("Filename: ")
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            if file == name:
+                pth = os.path.join(root, file)
+                check(file, pth)
+
+commands = {"update": update, "pack": pack, "checkall": checkAll, "check": checkcmd }
 
 
 def clear(): os.system("cls")
